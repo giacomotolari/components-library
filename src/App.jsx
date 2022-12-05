5968bdc8287c12adb8a3fcb0c60c2def16eb20e7
giacomotolari <tolarigiacomo@gmail.com>
2022-12-05 15:41:21 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.scss";
-import DetailListInput from "./components/detailListInput/DetailListInput";
+// import DetailListInput from "./components/detailListInput/DetailListInput";
 
 function App() {
-  const pokemonList = ["pichaku", "pokemon2", "pokemon3", "pokemon4"];
   return (
     <div className="App">
-      <DetailListInput placeholder="select pokemon" list={pokemonList} />
+      {/* <DetailListInput placeholder="select pokemon" list={list} /> */}
     </div>
   );
 }
