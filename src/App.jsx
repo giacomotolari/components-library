d59960b5461e2c15a61a01e15172f159bfac2275
giacomotolari <tolarigiacomo@gmail.com>
2022-12-05 15:35:41 +0100

    add Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.scss";
+import DetailListInput from "./components/detailListInput/DetailListInput";
 
 function App() {
-  return <div className="App"></div>;
+  const pokemonList = ["pichaku", "pokemon2", "pokemon3", "pokemon4"];
+  return (
+    <div className="App">
+      <DetailListInput placeholder="select pokemon" list={pokemonList} />
+    </div>
+  );
 }
 
 export default App;
